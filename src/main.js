aa0c6393bdc6f7f8791c696f5cb5697ff9587b6c
ruthkangyr <77619761+ruthkangyr@users.noreply.github.com>
2022-03-11 18:54:26 +0800

    edited

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
-import { BootstrapVue3 } from 'bootstrap-vue-3'
 import App from './App.vue'
 import router from './router/index.js'
 import store from './store'
-import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+
+import { BootstrapVue3 } from 'bootstrap-vue-3'
 import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css"
 
 createApp(App).use(store).use(router).use(BootstrapVue3).mount('#app')
