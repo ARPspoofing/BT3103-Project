00a99c66334399983d30aeb8465808b93d4199dc
Muhammad  Reyaaz <mohdreyaaz99@gmail.com>
2022-04-08 00:01:04 +0800

    student vuex mangamenet done

--- src/views/Students/ToDoView.vue
+++ src/views/Students/ToDoView.vue
@@ -252,7 +252,9 @@ export default {
 
       await updateDoc(ref, { Tasks: arrayUnion(newTask) });
 
-      this.task = newTask;
+      //non-vuex
+      //this.task = newTask;
+      this.SET_STUDENT_TASK(newTask)
       document.getElementById("comments").value = "";
     },
 
@@ -340,7 +342,21 @@ export default {
     }
     getComments();
     console.log(curr.comment);
-
+    /*
+       database.forEach((doc) => {
+               //Change to dynamic props later
+               if (doc.id == ("ToDo" + curr.task_id)) {
+                   var data = doc.data()
+                   console.log(data) 
+                   temp.push({
+                       name: data.taskname,
+                       duedate: data.duedate,
+                       documents: data.documents,
+                       comments: data.comments, 
+                       long: data.longdescription,
+                   })
+               }
+               }) 
     async function getTasksDetails() {
       //Change "To-Do" to props later
       const docRef = await doc(db, "Project", projectId);
@@ -356,21 +372,6 @@ export default {
           break;
         }
       }
-
-      /* database.forEach((doc) => {
-               //Change to dynamic props later
-               if (doc.id == ("ToDo" + curr.task_id)) {
-                   var data = doc.data()
-                   console.log(data) 
-                   temp.push({
-                       name: data.taskname,
-                       duedate: data.duedate,
-                       documents: data.documents,
-                       comments: data.comments, 
-                       long: data.longdescription,
-                   })
-               }
-               }) */
       console.log("currTask",currTask)
       curr.name = currTask.taskName;
       curr.duedate = currTask.taskDueDate;
@@ -383,6 +384,7 @@ export default {
         "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.";
     }
     getTasksDetails();
+    */
   },
 };
 </script>
